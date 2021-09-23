f77fe8a0dc769f540ebca1eebf268d7ba668aceb
Anupam Singh <anupamsinghbisht@gmail.com>
2021-09-23 15:32:07 +0530

    - updated test and sonar commands

--- src/sonar_scanner.bat
+++ src/sonar_scanner.bat
@@ -1,9 +1,11 @@
 dotnet build-server shutdown
 
-dotnet test -p:CollectCoverage=true -p:CoverletOutputFormat=opencover -p:Exclude=\"[Demo_xUnit_App.*]" -p:ExcludeByFile="**/*.xml"
-
-dotnet sonarscanner begin -k:"Demo-Unit-Test-Project" -d:sonar.login="1364ba86ba6c6186f5d8cf023487ca87a324dde6" -d:sonar.cs.opencover.reportsPaths="**/coverage.opencover.xml" -d:sonar.coverage.exclusions="Demo Console App/Demo Console App/CreditCardInterestCalculator/Data/*.xml, Demo xUnit App/coverage.opencover.xml"
+dotnet sonarscanner begin -k:"Demo-Unit-Test-Project" -d:sonar.login="1364ba86ba6c6186f5d8cf023487ca87a324dde6" -d:sonar.cs.opencover.reportsPaths="Demo xUnit App/TestResults/coverage.opencover.xml" -d:sonar.cs.vstest.reportsPaths="Demo xUnit App/TestResults/testresults.trx" -d:sonar.coverage.exclusions="Demo Console App/Demo Console App/CreditCardInterestCalculator/Data/*.xml, Demo xUnit App/coverage.opencover.xml"
 
 dotnet build
 
+dotnet test "Demo xUnit App/Demo xUnit App.csproj" --no-build --no-restore  -p:CollectCoverage=true -p:CoverletOutputFormat=opencover -p:CoverletOutput="TestResults/coverage.opencover.xml" -p:Exclude=\"[Demo_xUnit_App.*]" -p:ExcludeByFile="**/*.xml" --logger "trx;LogFileName=testresults.trx"
+
 dotnet sonarscanner end -d:sonar.login="1364ba86ba6c6186f5d8cf023487ca87a324dde6"
+
+pause
